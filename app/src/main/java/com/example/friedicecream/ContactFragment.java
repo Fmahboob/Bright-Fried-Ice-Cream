fc4f9630fd79711622c44666f9f4d42d462ed783
Farina Mahboob <W0766260@myscc.ca>
2021-11-14 12:34:47 -0500

    Finished Working with contact.xml create 4 button phone email a address facebook

--- app/src/main/java/com/example/friedicecream/ContactFragment.java
+++ app/src/main/java/com/example/friedicecream/ContactFragment.java
@@ -1,5 +1,8 @@
 package com.example.friedicecream;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +62,8 @@ public class ContactFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false);
+        View view = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        return view;
     }
 }
