091a93331eb975eaa675ec685cd36acb962a2770
Farina Mahboob <W0766260@myscc.ca>
2021-12-04 08:26:05 -0500

    Fixed view pager and add info in viewpager

--- app/src/main/java/com/example/friedicecream/ViewPager/CustomViewPageAdopter.java
+++ app/src/main/java/com/example/friedicecream/ViewPager/CustomViewPageAdopter.java
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
+import com.example.friedicecream.R;
+
 public class CustomViewPageAdopter extends FragmentStateAdapter {
     public CustomViewPageAdopter(@NonNull FragmentActivity fragmentActivity) {
         super(fragmentActivity);
@@ -13,11 +15,22 @@ public class CustomViewPageAdopter extends FragmentStateAdapter {
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        return null;
+        switch (position) {
+            case 0:
+                return FactFragment.newInstance("Cherry Rolled Ice Cream:", R.drawable.cherry, "Rolled ice cream filled with delicious pieces of fresh cherries.");
+            case 1:
+                return FactFragment.newInstance("Kiwi Rolled Ice Cream:d", R.drawable.kiwi, "Rolled Ice Cream filled with fine cut pieces of kiwi, topped with chocolate syrup.");
+            case 2: return FactFragment.newInstance("Pear Rolled Ice Cream:", R.drawable.pear, "Vanilla flavoured ice cream filled with finely cut pieces of savory pears");
+            case 3: return FactFragment.newInstance("Plum Rolled Ice Cream: ", R.drawable.plum, "Rolled ice cream filled with exquisite pieces of sweet plums");
+
+          case 4: return FactFragment.newInstance("Oreo Rolled Ice Cream:", R.drawable.oreo, "Vanilla flavoured ice cream topped with chunks of oreos and chocolate syrup, with one final piece of oreo at the top.");
+
+            default: return FactFragment.newInstance("404: Fragment not found", 21, "");
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 5;
     }
 }
